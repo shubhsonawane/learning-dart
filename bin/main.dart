e57a21569c68210aa38bd2918003f200cd2d7b00
unknown <shubhamsonawane76570@gmail.com>
2024-03-22 01:24:14 +0530

    data types

--- bin/main.dart
+++ bin/main.dart
@@ -1,20 +1,25 @@
 void main() {
-  print("hello dart");
+  // print("hello dart");
 
 // variables name and rules
-  var name = "shubham";
-  var f_name = "shubham";
-  var address1 = "nashik";
-  var $money = 100;
+  // var name = "shubham";
+  // var f_name = "shubham";
+  // var address1 = "nashik";
+  // var $money = 100;
 
   // Invalid Variable name
   // var 1person = "shubham";
   // var for = "male";
 
   // print variable name
-  print(name);
-  print(f_name);
+  // print(name);
+  // print(f_name);
 
   // built in DataTypes
-  
+  // int cart_item = 10;
+  // double price = 10.50;
+  // String city = "nashik";
+  // bool is_completed = false;
+  // var qty = 100;
+  // print(qty);
 }
