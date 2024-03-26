b7a46096575f0e4c623b9b2948acfd59f1caf089
unknown <shubhamsonawane76570@gmail.com>
2024-03-26 21:41:37 +0530

    break and continue

--- bin/main.dart
+++ bin/main.dart
@@ -356,5 +356,26 @@ void main() {
   // student.asMap().forEach((i, value) => print('$i = $value'));
 
   // #### break and continue
-  
+  // var is_fetching = true;
+  // var n = 0;
+  // while (is_fetching) {
+  //   n++;
+  //   if (n == 10) {
+  //     break;
+  //   }
+  //   print("Data Fetching.... $n");
+  // }
+  var is_fetching = true;
+  var n = 0;
+  while (is_fetching) {
+    n++;
+    if (n == 10) {
+      break;
+    }
+
+    if (n == 5) {
+      continue;
+    }
+    print("Data Fetching.... $n");
+  }
 }
